ffc8e55c5cc48860806df923d87d789dc909e271
michele.carbonera <michele.carbonera@unimib.it>
2022-01-09 17:30:57 +0100

    Add dataset analysis +PCA files

---
+++ Asteroids_analysis_PCA.R
@@ -0,0 +1,8 @@
+setwd("~/Github/AsteroidsClassification")
+install.packages(c("FactoMineR", "factoextra"))
+library("FactoMineR")
+library("factoextra")
+
+#load dataset RObject as asteroids_data
+load("asteroids_dataset.RData")
+ 
---
+++ Asteroids_analysis_dataset.R
@@ -0,0 +1,4 @@
+setwd("~/Github/AsteroidsClassification")
+#load dataset RObject as asteroids_data
+load("asteroids_dataset.RData")
+
