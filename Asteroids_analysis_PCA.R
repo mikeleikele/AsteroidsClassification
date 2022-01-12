4e862fef7cf86e58ce53463692a6883e3e5050aa
Antonino <antonino.tramontana@protonmail.com>
2022-01-12 14:38:44 +0100

    dataset initial analysis

---
+++ Asteroids_analysis_PCA.R
@@ -0,0 +1,49 @@
+setwd("~/Github/AsteroidsClassification")
+install.packages(c("FactoMineR", "factoextra"))
+library("FactoMineR")
+library("factoextra")
+
+#load dataset RObject as asteroids_data
+load("asteroids_dataset.RData")
+
+#PCA feature extration
+
+col_active = c("Absolute.Magnitude","Est.Dia.in.KM.min.","Est.Dia.in.KM.max.","Est.Dia.in.M.min.","Est.Dia.in.M.max.","Est.Dia.in.Miles.min.","Est.Dia.in.Miles.max.","Est.Dia.in.Feet.min.","Relative.Velocity.km.per.sec","Epoch.Date.Close.Approach","Relative.Velocity.km.per.hr","Miles.per.hour","Miss.Dist..Astronomical.","Miss.Dist..lunar.","Miss.Dist..kilometers.","Miss.Dist..miles.","Orbit.Uncertainity","Minimum.Orbit.Intersection","Jupiter.Tisserand.Invariant","Epoch.Osculation","Eccentricity","Semi.Major.Axis","Inclination","Asc.Node.Longitude","Orbital.Period","Perihelion.Distance","Perihelion.Arg","Aphelion.Dist","Perihelion.Time","Mean.Anomaly","Mean.Motion")
+asteroids_data.active <- asteroids_data[,col_active]
+
+asteroids_pca = PCA(asteroids_data.active, graph = TRUE)
+
+asteroids_pca.eig_val <- get_eigenvalue(asteroids_pca)
+
+fviz_eig(asteroids_pca, addlabels = TRUE, ylim = c(0, 50))
+fviz_screeplot(asteroids_pca)
+
+#var
+var <- get_pca_var(asteroids_pca)
+fviz_pca_var(asteroids_pca, col.var = "black")
+var$coord
+var$cos2
+var$contrib
+
+
+#istanze
+ind <- get_pca_ind(asteroids_pca)
+fviz_pca_ind(asteroids_pca)
+
+fviz_pca_ind(asteroids_pca, col.ind = "cos2",
+  gradient.cols = c("#00AFBB", "#E7B800", "#FC4E07"),
+    repel = TRUE # Avoid text overlapping (slow if many points)
+)
+
+
+fviz_pca_biplot(asteroids_pca,
+                geom.ind = "point",
+                col.ind = asteroids_data$Hazardous,
+                palette = c("#00AFBB", "#E7B800", "#FC4E07"),
+                addEllipses = TRUE,
+                legend.title = "Groups"
+)
+
+var$coord
+var$cos2
+var$contrib
--- CODE_asteroids_analysis_dataset.R
+++ CODE_asteroids_analysis_dataset.R
@@ -1,10 +1,10 @@
 setwd("C:/Users/Gasatoz/Github/AsteroidsClassification")
 #load dataset RObject as asteroids_data
-load("asteroids_dataset.RData")
+load("DATA_asteroids_dataset.RData")
 
 ####### Preliminary Analysis ############
 
-dim(asteroids_data)
+dim(asteroids_dataset)
 
 
 #### get types of each variable
@@ -17,89 +17,95 @@ levels(asteroids_data$Hazardous)
 
 class_frequencies = table(asteroids_data$Hazardous)
 
+
+asteroid_type = table(asteroids_data$Classification)
+
+asteroid_type
+
 ## pie chart 
 pie(class_frequencies)
+pie(asteroid_type)
 
+## bar plot
+barplot(table(asteroids_data$Hazardous))
 
+barplot(asteroid_type)
 # get various statistics of each covariate
 ### mean, sd, var, min, max, median, range, quantile
-covariates = asteroids_data[,c("Est.Dia.in.KM.min." , "Est.Dia.in.M.min." , "Est.Dia.in.Miles.min." , "Est.Dia.in.Feet.min.",
-              "Est.Dia.in.KM.max." , "Est.Dia.in.M.max." , "Est.Dia.in.Miles.max." , "Est.Dia.in.Feet.max.",
-              "Relative.Velocity.km.per.sec", "Relative.Velocity.km.per.hr",  "Miles.per.hour",
-              "Miss.Dist..Astronomical.", "Miss.Dist..lunar." , "Miss.Dist..miles." , 
-              "Minimum.Orbit.Intersection" , "Jupiter.Tisserand.Invariant" , "Epoch.Osculation" ,
-              "Eccentricity" , "Semi.Major.Axis" ,"Inclination",
-              "Asc.Node.Longitude", "Orbital.Period" , "Perihelion.Distance",
-              "Perihelion.Arg", "Mean.Anomaly", "Mean.Motion"        
-              )]
+numeric_covariates = asteroids_data[,c("Orbit.Axis..AU." , "Orbit.Eccentricity" , "Orbit.Inclination..deg." , "Perihelion.Argument..deg.",
+                               "Node.Longitude..deg." , "Mean.Anomoly..deg." , "Perihelion.Distance..AU." , "Aphelion.Distance..AU.",
+                               "Orbital.Period..yr.", "Minimum.Orbit.Intersection.Distance..AU.",  "Asteroid.Magnitude"
+                                     )]
+factor_covariates = asteroids_data[,c("Orbital.Reference", "Classification", "Epoch..TDB." )]
+rows = c("mean", "sd" , "var", "min", "max", "median") # "range", "quantile" )
 
-for x in covariates ){
-  mean((x))
-  sd((x))
-  var((x))
-  min((x))
-  max((x))
-  median((x))
-  range((x))
-  quantile((x))
+columns = c("Orbit.Axis..AU." , "Orbit.Eccentricity" , "Orbit.Inclination..deg." , "Perihelion.Argument..deg.",
+         "Node.Longitude..deg." , "Mean.Anomoly..deg." , "Perihelion.Distance..AU." , "Aphelion.Distance..AU.",
+         "Orbital.Period..yr.", "Minimum.Orbit.Intersection.Distance..AU.",  "Asteroid.Magnitude"
+)
+stats <- data.frame(matrix(nrow = length(rows), ncol = length(columns))) 
+
+# assign column names
+colnames(stats) = columns
+rownames(stats) = rows
+
+i <- 1
+
+for( x in numeric_covariates ){
+  stats['mean', columns[i] ] <- mean((x))
+  stats[ 'sd' , columns[i] ] <- sd((x))
+  stats[ 'var' , columns[i] ] <- var((x))
+  stats[ 'min' , columns[i] ] <- min((x))
+  stats['max' , columns[i] ] <- max((x))
+  stats[ 'median' , columns[i]] <- median((x))
+  #stats[ i, 'range'] <- range((x))
+  #stats[columns[i]]['quantile'] = quantile((x))
+  i <- i + 1 
   }
 
+
+stats
 ## mean
+stats[columns[1]]['mean']
 
 #### make boxplot of each covariate
 
-hist(asteroids_data$Absolute.Magnitude)
-
-x = asteroids_data[, c("Est.Dia.in.KM.min." , "Est.Dia.in.M.min." , "Est.Dia.in.Miles.min." , "Est.Dia.in.Feet.min.",
-                       "Est.Dia.in.KM.max." , "Est.Dia.in.M.max." , "Est.Dia.in.Miles.max." , "Est.Dia.in.Feet.max.",
-                       "Relative.Velocity.km.per.sec", "Relative.Velocity.km.per.hr",  "Miles.per.hour",
-                       "Miss.Dist..Astronomical.", "Miss.Dist..lunar." , "Miss.Dist..miles." , 
-                       "Minimum.Orbit.Intersection" , "Jupiter.Tisserand.Invariant" , 
-                       "Epoch.Osculation", "Eccentricity" , "Semi.Major.Axis" ,
-                       "Inclination", "Asc.Node.Longitude", "Orbital.Period" , 
-                       "Perihelion.Distance", "Perihelion.Arg", "Mean.Anomaly" )]
+x = numeric_covariates 
 
 par(mfrow = c (2,4))
 
 for (i in 1:8) {
-  
-  boxplot(x[,i], main=names(x)[i]) 
-  
-  }
-
-x = asteroids_data[, c("Relative.Velocity.km.per.sec", "Relative.Velocity.km.per.hr",  "Miles.per.hour",
-                       "Miss.Dist..Astronomical.", "Miss.Dist..lunar." , "Miss.Dist..miles." , 
-                       "Minimum.Orbit.Intersection" , "Jupiter.Tisserand.Invariant" , "Epoch.Osculation"
-                       )
-                   ]
-
-par(mfrow = c (3,3))
-
-for (i in 1:9) {
-  
-  boxplot(x[,i], main=names(x)[i]) 
-  
-  }
-
-x = asteroids_data[, c("Mean.Motion", "Eccentricity" , "Semi.Major.Axis" ,
-                       "Inclination", "Asc.Node.Longitude", "Orbital.Period" , 
-                       "Perihelion.Distance", "Perihelion.Arg", "Mean.Anomaly" 
-                        )
-                   ]
-
-par(mfrow = c (3,3))
-
-for (i in 1:9) {
   
   boxplot(x[,i], main=names(x)[i]) 
   
 }
 
-# histograms of each covariate
-hist(asteroids_data$Absolute.Magnitude)
+par(mfrow = c (1,3))
 
-x = asteroids_data[, c("Est.Dia.in.KM.min." , "Est.Dia.in.M.min." , "Est.Dia.in.Miles.min." , "Est.Dia.in.Feet.min.",
-                       "Est.Dia.in.KM.max." , "Est.Dia.in.M.max." , "Est.Dia.in.Miles.max." , "Est.Dia.in.Feet.max.")]
+for (i in 9:11) {
+  
+  boxplot(x[,i], main=names(x)[i]) 
+  
+}
+
+### box plot factor variables
+
+x = factor_covariates 
+
+
+par(mfrow = c (1,3))
+
+for (i in 1:3) {
+  
+  boxplot(x[,i], main=names(x)[i]) 
+  
+}
+
+
+
+### histogram numeric covariates
+
+x = numeric_covariates 
 
 par(mfrow = c (2,4))
 
@@ -107,34 +113,15 @@ for (i in 1:8) {
   
   hist(x[,i], main=names(x)[i]) 
   
-  }
+}
 
-x = asteroids_data[, c("Relative.Velocity.km.per.sec", "Relative.Velocity.km.per.hr",  "Miles.per.hour",
-                       "Miss.Dist..Astronomical.", "Miss.Dist..lunar." , "Miss.Dist..miles." , 
-                       "Minimum.Orbit.Intersection" , "Jupiter.Tisserand.Invariant" , "Epoch.Osculation"
-                   )]
+par(mfrow = c (1,3))
 
-par(mfrow = c (3,3))
-
-for (i in 1:9) {
-  
-  hist(x[,i], main=names(x)[i]) 
-
-  }
-
-x = asteroids_data[, c("Eccentricity" , "Semi.Major.Axis" ,"Inclination",
-                       "Asc.Node.Longitude", "Orbital.Period" , "Perihelion.Distance",
-                       "Perihelion.Arg", "Mean.Anomaly", "Mean.Motion" 
-                       
-                       )
-                   ]
-
-
-for (i in 1:9) {
+for (i in 9:11) {
   
   hist(x[,i], main=names(x)[i]) 
   
-  }
+}
 
 
 
@@ -143,13 +130,12 @@ library(caret)
 #### multivariate plots
 par(mfrow = c (1,1))
 
-plot(x=asteroids_data$Mean.Anomaly, y=asteroids_data$Mean.Motion, col=asteroids_data$Hazardous)
-plot(x=asteroids_data$Perielion.Distance, y=asteroids_data$Eccentricity, col=asteroids_data$Hazardous)
-
+plot(x=asteroids_data$Node.Longitude..deg. , y=asteroids_data$Minimum.Orbit.Intersection.Distance..AU., col=asteroids_data$Hazardous)
+plot(x=asteroids_data$Mean.Anomoly..deg., y=asteroids_data$Eccentricity, col=asteroids_data$Hazardous)
 
 ### ggplot
 
-library(ggplot2)
+library(ggplot2) 
 library(ggExtra)
 
 
@@ -160,5 +146,12 @@ p
 # Plot the scatter plot with marginal histograms
 ggMarginal(p, type = "histogram")
 
-ggplot(asteroids_data, aes(Orbital.Period)) + geom_density(aes(fill=factor(Hazardous), alpha=0.75))
-ggplot(asteroids_data, aes(x=Inclination, y=Hazardous, size = pop )) + geom_point(alpha=0.7)
+ggplot(asteroids_data, aes(Perihelion.Argument..deg.)) + geom_density(aes(fill=factor(Hazardous), alpha=0.75))
+ggplot(asteroids_data, aes(x=Orbital.Period..yr., y=Hazardous )) + geom_point(alpha=0.7)
+
+
+### best variable up - Jupiter Tisserand - mean Motion  Miss dist lunar
+#pca 1
+#  apelion dist , orbital period , semi major axis
+# pca 2
+# minimum orbit intersection perihelion arg , absolute magnituted uncertainity
