e8c8112f8f836b0d36d5fa4c21cb9d552697ea18
Antonino <antonino.tramontana@protonmail.com>
2022-01-13 17:28:27 +0100

    added plots

--- CODE_asteroids_analysis_KNN.R
+++ CODE_asteroids_analysis_KNN.R
@@ -21,20 +21,51 @@ fit
 
 fit$cluster
 
+### barplot cluster frequency
+
+img_name_plot <- paste("IMG_asteroids_model_KNN_", "barplot_k_4", ".png", sep = "")
+png(img_name_plot,res = 800, height = 10, width = 15, unit='in')
+
+barplot(table(fit$cluster), main = "Frequency by cluster",
+        col = rainbow(4))
+
+dev.off()
+
+img_name_plot <- paste("IMG_asteroids_model_KNN_", "barplot_k_6", ".png", sep = "")
+png(img_name_plot, res = 800, height = 10, width = 15, unit='in')
+
+
+fit6 = kmeans(numeric_covariates, 6)
+fit6
+
+
+barplot(table(fit6$cluster), main = "Frequency by cluster",
+        col = rainbow(6))
+
+dev.off()
+
+
+### barplot Classification class
+
+barplot(table(asteroids_data$Classification), main= "Frequency Asteroids Classes", col=rainbow(4))
 
 kms = silhouette(fit$cluster,dist(numeric_covariates))
 kms
 plot(kms)
 
+img_name_plot <- paste("IMG_asteroids_model_KNN_", "silouhette_score_per_k", ".png", sep = "")
+png(img_name_plot,res = 800, height = 10, width = 15, unit='in')
+
 silhouette_score <- function(k){
-  km <- kmeans(df, centers = k, nstart=25)
-  ss <- silhouette(km$cluster, dist(df))
+  km <- kmeans(numeric_covariates, centers = k, nstart=25)
+  ss <- silhouette(km$cluster, dist(numeric_covariates))
   mean(ss[, 3])
 }
 k <- 2:10
 avg_sil <- sapply(k, silhouette_score)
 plot(k, type='b', avg_sil, xlab='Number of clusters', ylab='Average Silhouette Scores', frame=FALSE)
 
+dev.off()
 
 
 ### dissimilarity clusters
@@ -42,7 +73,7 @@ plot(k, type='b', avg_sil, xlab='Number of clusters', ylab='Average Silhouette S
 
 library('seriation')
 
-dissplot(dist(numeric_covariates), labels=fit$cluster, options=list(main='Kmeans Clustering with K=4'))
+dissplot(dist(numeric_covariates), labels=fit6$cluster, options=list(main='Kmeans Clustering with K=6'))
 
 
 ### fine tuninig
@@ -54,11 +85,13 @@ fit$cluster
 cluser.evaluation(ground_truth, fit$cluser)
          
          
-### varying of the silhouette score at the number of clusters
+### measuring the silhouette score at the number of clusters
 
 library(fpc)
 nk = 2:4
 set.seed(22)
+
+
 Sw < sapply(nk, function(k) { cluster.stats(dist(numeric_covariates),kmeans(numeric_covariates, centers=k)$cluster)$avg.silwidth})
 SW
 plot(SW, type='I', xlab='number of clusters', ylab='average silhoutte')
